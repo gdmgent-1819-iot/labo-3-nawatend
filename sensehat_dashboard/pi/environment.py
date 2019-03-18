2701f0e19d4a99d1fd416f9586cfc19c48eee314
nawatend <32384185+nawatend@users.noreply.github.com>
2019-03-18 19:03:51 +0100

    Update environment.py

--- sensehat_dashboard/pi/environment.py
+++ sensehat_dashboard/pi/environment.py
@@ -4,7 +4,14 @@ from firebase_admin import db
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('./credential.json')
-
+config = {
+    apiKey: "AIzaSyAfx2W4GkPmsqV0Ag6hK6VQQjAAmvI3RqA",
+    authDomain: "environment-pi.firebaseapp.com",
+    databaseURL: "https://environment-pi.firebaseio.com",
+    projectId: "environment-pi",
+    storageBucket: "environment-pi.appspot.com",
+    messagingSenderId: "484802952614"
+}
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://environment-pi.firebaseio.com/'
